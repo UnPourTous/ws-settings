dfbb7d3a16ec0b4f1c238803176ef02d83fd8c34
erichua23 <huatingzl@gmail.com>
2019-01-22 15:18:42 +0800

    WS-2018.1.1 <erichua@erichuas-MacMini.local Create baseRefactoring.xml, vim_settings.xml, IntelliLang.xml, ide.general.xml, _@user_Default.icls, print.xml, project.default.xml, erichua.icls, diff.xml, git.xml, other.xml, Default.xml, file.template.settings.xml, xpath.xml, find.xml, ui.lnf.xml, filetypes.xml, code.style.schemes.xml, plugin_ui.xml, Deployment.xml, watcherDefaultTasks.xml, notifications.xml, path.macros.xml, github_settings.xml, JavaScript File.js, Default.xml, werich_rn.xml, laf.xml, Default _1_.xml, Mac OS X 10_5_ copy.xml, web-browsers.xml, abbreviations.xml, macros.xml, encoding.xml, vcs.xml, editor.codeinsight.xml, keymap.xml, editor.xml, usageView.xml, applicationLibraries.xml, webServers.xml, debugger.xml

---
+++ fileTemplates/internal/JavaScript File.js
@@ -0,0 +1,16 @@
+/**
+ * Created by ${USER} on ${DATE}.
+ */
+
+import React, { Component } from 'react'
+import {
+  View
+} from 'react-native'
+export default class  extends Component {
+  static propTypes = {
+
+  }
+  render () {
+
+  }
+}
